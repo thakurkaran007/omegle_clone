3868f14505ebd16c1580bb260b8d99234577d898
Karan Thakur <thakurkaran0345@gmail.com>
2025-01-29 08:51:58 +0530

    init

---
+++ apps/web/actions/login.ts
@@ -0,0 +1,66 @@
+"use server";
+import { signIn } from "@/auth";
+import { getUserByEmail } from "@/data/user";
+import { sendVerificationMail } from "@/lib/mail";
+import { generatetVerificationToken } from "@/lib/tokens";
+import { DEFAULT_LOGIN_REDIRECT } from "@/route";
+import { LoginSchema } from "@/schema";
+import { AuthError } from "next-auth";
+import * as z from "zod";
+import bcrypt from "bcryptjs";
+import { getVerificationTokenByEmail } from "@/data/verification-token";
+import { db } from "@repo/db/src";
+
+export const login = async(values: z.infer<typeof LoginSchema>) => {
+    const validation = LoginSchema.safeParse(values);
+    if (!validation.success) {
+        return {
+            error: "Invalid input"
+        }
+    }
+    const { email, password } = validation.data;
+
+    const existingUser = await getUserByEmail(email);
+    if (!existingUser || !existingUser.password) {
+        return { error: "Email does not exist" };
+    }
+
+    const passVerify = bcrypt.compareSync(password, existingUser?.password);
+    if (!passVerify) {
+        return { error: "Wrong Password" };
+    }
+    if (!existingUser.emailVerified) {
+        const verificationToken = await generatetVerificationToken(email);
+        await sendVerificationMail(email, verificationToken.token);
+        return { success: "Confirmation email Sent" };
+    } else {
+        const token = await getVerificationTokenByEmail(email);
+        if (token) {
+            const hasExpired = new Date(token.expires) < new Date();
+            if (hasExpired) {
+                await db.verificationToken.delete({ where: { id: token.id } });
+                const verificationToken = await generatetVerificationToken(email);
+                await sendVerificationMail(email, verificationToken.token);
+                return { success: "Confirmation email Sent" };
+            }
+        }
+    }
+    
+    try {
+        await signIn("credentials", {
+            email,  
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT
+        })
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Invalid Credentials" }
+                default:
+                    return { error: "Something Went Wrong" }
+            }
+        }
+        throw error;
+    }
+}
---
+++ packages/db/prisma/migrations/20250129031748_init/migration.sql
@@ -0,0 +1,34 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "password" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_email_token_key" ON "VerificationToken"("email", "token");
