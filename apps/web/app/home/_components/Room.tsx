db430db411e61e8e3badee41853d23d27b6ba4f0
Karan Thakur <144684608+thakurkaran007@users.noreply.github.com>
2025-02-06 15:11:29 +0530

    Update Room.tsx

--- apps/web/app/home/_components/Room.tsx
+++ apps/web/app/home/_components/Room.tsx
@@ -44,7 +44,7 @@ const Room = () => {
           ],
           username: "openrelayproject",
           credential: "openrelayproject",
-        },,
+        },
         {
           urls: [
             "stun:stun.l.google.com:19302", // Google STUN server
