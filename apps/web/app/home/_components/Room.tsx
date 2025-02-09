971b108b7609cc4edb369c0eb9d20cec3a35c38a
Karan Thakur <144684608+thakurkaran007@users.noreply.github.com>
2025-02-09 21:15:26 +0530

    Update Room.tsx

--- apps/web/app/home/_components/Room.tsx
+++ apps/web/app/home/_components/Room.tsx
@@ -437,14 +437,14 @@ const Room = () => {
                     <Button className="flex-grow ml-2 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" disabled={(onGoinngCall ? disabled : false)} onClick={onGoinngCall ? stop : New}>{onGoinngCall ? "Stop" : "New"}</Button>
                     <input 
                         type="text" 
-                        disabled={(onGoinngCall ? (disabled ? false : true) : true)}
+                        disabled={(onGoinngCall ? disabled : false)}
                         value={message} 
                         onChange={(e) => setMessage(e.target.value)}
                         className="flex-1 p-2 border rounded-md outline-none text-sm" 
                         placeholder="Type a message..."
                         onKeyDown={(e) => e.key === "Enter" && sendMessage()}
                     />
-                    <Button onClick={() => sendMessage()} className="ml-2 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" disabled={(onGoinngCall ? (disabled ? false : true) : true)}>Send</Button>
+                    <Button onClick={() => sendMessage()} className="ml-2 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" disabled={(onGoinngCall ? disabled : false)}>Send</Button>
                 </div>
             </div>
         </div>
