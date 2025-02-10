bae8656be1a33b304f704adfad2ad9f95a4cb17f
Karan Thakur <144684608+thakurkaran007@users.noreply.github.com>
2025-02-10 20:29:12 +0530

    Update send-otp.ts

--- apps/web/actions/send-otp.ts
+++ apps/web/actions/send-otp.ts
@@ -1,15 +1,37 @@
-"use server"
+"use server";
 
 import { getUserByEmail } from "@/data/user";
-import { sendOtp } from "@/lib/mail"
+import { sendOtp } from "@/lib/mail";
 
-export const send = async (email: string) => {
+export const send = async (email: string, token: string) => {
+    const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY!;
+
     try {
-        const existingUser = await getUserByEmail(email);
-        if (existingUser) return { error: "Email Already in use" };
+        // Verify reCAPTCHA
+        const response = await fetch(
+            "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: new URLSearchParams({
+                    secret: RECAPTCHA_SECRET_KEY,
+                    response: token,
+                }),
+            }
+        );
+
+        const { success } = await response.json();
+        if (!success) return { error: "Captcha verification failed" };
+
+        // Check if user exists
+        if (await getUserByEmail(email)) {
+            return { error: "Email already in use" };
+        }
+
+        // Send OTP
         await sendOtp(email);
         return { success: "OTP sent" };
     } catch (error) {
-        return { error }
+        return { error: "Something went wrong" };
     }
-}
+};
