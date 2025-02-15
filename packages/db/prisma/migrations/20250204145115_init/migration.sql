97234fe6da7ae87a774af48715fed2e54ed79e40
Karan Thakur <144684608+thakurkaran007@users.noreply.github.com>
2025-02-15 06:55:22 +0530

    Update migration.sql

--- packages/db/prisma/migrations/20250204145115_init/migration.sql
+++ packages/db/prisma/migrations/20250204145115_init/migration.sql
@@ -15,6 +15,24 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
+CREATE TABLE "accounts" (
+    "_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("_id")
+);
+
+-- CreateTable
 CREATE TABLE "VerificationToken" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
@@ -28,7 +46,13 @@ CREATE TABLE "VerificationToken" (
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "VerificationToken_email_token_key" ON "VerificationToken"("email", "token");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
