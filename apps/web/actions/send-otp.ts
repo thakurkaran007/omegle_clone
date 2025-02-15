510ff7356e9e0d3fa809bb96faf1d04efa4bb307
Karan Thakur <144684608+thakurkaran007@users.noreply.github.com>
2025-02-15 11:01:51 +0530

    Update send-otp.ts

--- apps/web/actions/send-otp.ts
+++ apps/web/actions/send-otp.ts
@@ -2,27 +2,16 @@
 
 import { getUserByEmail } from "@/data/user";
 import { sendOtp } from "@/lib/mail";
+import { validateCaptcha } from "@/lib/validateCaptcha";
 
 export const send = async (email: string, token: string) => {
-    const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY!;
 
     try {
         // Verify reCAPTCHA
-        const response = await fetch(
-            "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-            {
-                method: "POST",
-                headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                body: new URLSearchParams({
-                    secret: RECAPTCHA_SECRET_KEY,
-                    response: token,
-                }),
-            }
-        );
-
-        const { success } = await response.json();
-        if (!success) return { error: "Captcha verification failed" };
-
+        const validateCap = await validateCaptcha(token);
+        if (!validateCap) {
+            return { error: "Captcha Validation Failed" };
+        }
         // Check if user exists
         if (await getUserByEmail(email)) {
             return { error: "Email already in use" };
